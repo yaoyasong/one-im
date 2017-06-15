06190e55b2ee95221b5b58bb3deb2d807a65623c
yaoyasong <yaoyasong@gmail.com>
2017-06-15 11:16:39 +0800

    add crash report

--- app/setup.js
+++ app/setup.js
@@ -1,9 +1,15 @@
 import App from "./App";
 import LeanCloudAPI from './common/utils/leanCloudAPI';
+import toastUtils from "./common/utils/toastUtils";
 
 function setup() {
   //init leancloud sdk
   LeanCloudAPI.init();
+  //report error
+  require('ErrorUtils').setGlobalHandler((err) => {
+    toastUtils.showLong(err);
+  });
+
   return App;
 }
 export default setup;
